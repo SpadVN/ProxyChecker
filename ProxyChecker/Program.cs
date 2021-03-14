41e34e446e09bff92cbb356f627fda96568c56e2
SpadVN <60429438+SpadVN@users.noreply.github.com>
2021-03-14 16:01:03 +0100

    Now the proxy output text change color to red if it's not working and green if it is.

--- ProxyChecker/Program.cs
+++ ProxyChecker/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Leaf.xNet;
 
 namespace ProxyChecker
 {
@@ -10,6 +13,46 @@ namespace ProxyChecker
     {
         static void Main(string[] args)
         {
+            Console.Title = "Proxy Checker by DevXYZ#9788";
+            var proxies = File.ReadAllLines("proxies.txt");
+            var threads = 50;
+            var requestTimeout = 5 * 1000;
+            var proxyTimeout = 2 * 1000;
+            var proxyType = ProxyType.Socks4;
+
+            var good = 0;
+
+            Parallel.ForEach(proxies, new ParallelOptions() {
+                MaxDegreeOfParallelism = threads }, proxy =>
+                {
+                    try
+                    {
+                        using (var request = new HttpRequest())
+                        {
+                            request.ConnectTimeout = requestTimeout;
+                            request.Proxy = ProxyClient.Parse(proxyType, proxy);
+                            request.Proxy.ConnectTimeout = proxyTimeout;
+
+                            request.Get("http://azenv.net/");
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("[GOOD PROXY]" + proxy);
+                            Console.ResetColor();
+
+                            Interlocked.Increment(ref good);
+                            File.AppendAllText("good.txt", proxyTimeout + Environment.NewLine);
+                        }
+                    }
+                    catch 
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[BAD PROXY]" + proxy);
+                        Console.ResetColor();
+                    }
+                });
+
+            Console.WriteLine("Done checking!\n\n Good proxies: " + proxies);
+            Console.ReadKey();
         }
     }
 }
